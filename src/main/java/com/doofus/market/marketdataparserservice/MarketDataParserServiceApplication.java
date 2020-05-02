c4c3c40389f149af59f264b22892ac1799d9927b
Kndarp Patel <kndarppatel@gmail.com>
2020-05-02 17:12:23 +0200

    Initial commit

---
+++ src/main/java/com/doofus/market/marketdataparserservice/MarketDataParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.doofus.market.marketdataparserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketDataParserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketDataParserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doofus/market/marketdataparserservice/MarketDataParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doofus.market.marketdataparserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataParserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
